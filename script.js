7d27e94dc261639634d6b5a6713085106254a131
Dmitri Gorun <iii448@mail.ru>
2021-07-30 01:41:29 +0500

    lesson01-alert-consoleLog

---
+++ script.js
@@ -0,0 +1,10 @@
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+
+alert('Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, cumque ratione. Nisi temporibus');
+
+console.log('Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, cumque ratione. Nisi temporibus');
