88924c7e785b2af9aa14395a076f357678a02f6c
Dmitri Gorun <iii448@mail.ru>
2021-07-31 22:14:33 +0500

    lesson02-typeData-operators-methods -fixed

--- script.js
+++ script.js
@@ -7,16 +7,13 @@ let period = 12;
 let budgetMonth = 66000;
 let budgetDay = budgetMonth / 30;
 
-alert('Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, cumque ratione. Nisi temporibus');
-
-console.log('Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, cumque ratione. Nisi temporibus');
-
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
-addExpenses = addExpenses.toLowerCase().replace(/,/g, '').split(' ');
-console.log(addExpenses);
+
+let arrAddExpenses = addExpenses.toLowerCase().split(', ');
+console.log(arrAddExpenses);
 console.log(budgetDay);
