8ba89cae7708aa14e1ed7c3662c113a2957695f7
Dmitri Gorun <iii448@mail.ru>
2021-08-18 19:25:50 +0500

    lesson15 -ES6-parts-2-fixed

--- script_lesson15_part_2.js
+++ script_lesson15_part_2.js
@@ -1,28 +1,19 @@
 'use strict';
 
-
 class First {
   hello() {
     console.log('Привет я метод родителя!');
   }
 }
-const classFirst = new First();
 
+// ===================================================
 
 class Second extends First {
   hello() {
-    classFirst.hello();
+    super.hello();
     console.log('А я наследуемый метод!');
   }
 }
 const classSecond = new Second();
 
 classSecond.hello();
-
-
-
-
-
-
-
-
